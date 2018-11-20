f23eaee490ca34d76b3cc788fc9178ef3250df47
root <root@localhost.localdomain>
2018-11-20 06:00:19 -0500

    Add comments on Calc_Area File

--- Calc_Area.java
+++ Calc_Area.java
@@ -3,18 +3,23 @@ import java.util.Scanner;
 public class Calc_Area{
     public static void main(String[] args) {
         
+        // This Line Always Should be written, when you want to read input variables
         Scanner input = new Scanner(System.in);
 
+        // Variables Creation
         int Length = 0;
         int Width = 0;
         float Area = 0;
 
+        //Here we read int from screen 
         System.out.print("Enter Length: ");
         Length = input.nextInt();
 
+        //Here we read int from screen
         System.out.print("Enter Width: ");
         Width = input.nextInt();
 
+        //Here we make calculations
         Area = Length * Width;
 
         System.out.println("The Area of Rectangular is: "+ Area);
